6e1c4629242242921708e1c021de1cae4867bfcd
Estudiante <Estudiante@MULTIMEDIA-16>
2024-04-11 11:20:42 -0400

    AVANCE EN CLASES CON EL GARCIA XDXDXDDDXXDDD

--- src/taller2/Persona.java
+++ src/taller2/Persona.java
@@ -2,90 +2,91 @@ package taller2;
 
 import java.util.List;
 
-public class progenitoras implements List<descendientes> {
+public class Persona {
 	
 	private String nombre;
 	private String rut;
 	private int edad;
 	private String rut_madre;
+	private String rut_padre;
 	private String rut_pareja;
-	private List<descendientes> listaDescendientes; 
+	private List<Persona> listaDescendientes; 
 	
-	public progenitoras(String nombre, String rut, int edad, String rut_madre, String rut_pareja) {
-		
+	public Persona(String nombre, String rut, int edad, String rut_madre) {
 		
 		this.nombre = nombre;
 		this.rut = rut;
 		this.edad = edad;
 		this.rut_madre = rut_madre;
+		this.rut_padre = rut_padre;
 		this.rut_pareja = rut_pareja;
-
+		this.listaDescendientes = listaDescendientes;
+		
+		if (rut_pareja == null) {
+			
+			rut_pareja = "NO-IDENTIFICA";
+		}
+		
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
-	public List<descendientes> getListaDescendientes() {
-		return listaDescendientes;
-	}
-
-
-	public void setListaDescendientes(List<descendientes> listaDescendientes) {
-		this.listaDescendientes = listaDescendientes;
-	}
-
 
 	public String getRut() {
 		return rut;
 	}
 
-
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
-
 
 	public int getEdad() {
 		return edad;
 	}
 
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
 
 	public String getRut_madre() {
 		return rut_madre;
 	}
 
-
 	public void setRut_madre(String rut_madre) {
 		this.rut_madre = rut_madre;
 	}
 
+	public String getRut_padre() {
+		return rut_padre;
+	}
+
+	public void setRut_padre(String rut_padre) {
+		this.rut_padre = rut_padre;
+	}
 
 	public String getRut_pareja() {
 		return rut_pareja;
 	}
 
-
 	public void setRut_pareja(String rut_pareja) {
 		this.rut_pareja = rut_pareja;
 	}
 
+	public List<Persona> getListaDescendientes() {
+		return listaDescendientes;
+	}
 
-	@Override
+	public void setListaDescendientes(List<Persona> listaDescendientes) {
+		this.listaDescendientes = listaDescendientes;
+	}
 	public String toString() {
-		return "progenitoras [nombre=" + nombre + ", rut=" + rut + ", edad=" + edad + ", rut_madre=" + rut_madre
-				+ ", rut_pareja=" + rut_pareja + ", listaDescendientes=" + listaDescendientes + "]";
+		return "Persona [nombre=" + nombre + ", rut=" + rut + ", edad=" + edad + ", rut_madre=" + rut_madre
+				+ ", rut_padre=" + rut_padre + ", rut_pareja=" + rut_pareja + ", listaDescendientes="
+				+ listaDescendientes + "]";
 	}
 }
--- src/taller2/taller2.java
+++ src/taller2/taller2.java
@@ -2,6 +2,7 @@ package taller2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 public class taller2 {
@@ -11,7 +12,8 @@ public class taller2 {
 		
 		File archivo_descendientes = new File("descendientes.txt");
 		Scanner archivo1 = new Scanner(archivo_descendientes);
-		List<descendientes> descendientes = new ArrayList<descendientes>();
+		List<Persona> descendientes = new ArrayList<Persona>();
+		int i=0;
 		while (archivo1.hasNextLine()) {
 			
 			
@@ -23,14 +25,17 @@ public class taller2 {
 			String rut_madre = partes[3];
 			String rut_padre = partes[4];
 			
-			descendientes.add(new descendientes(nombre,edad,rut,rut_madre,rut_padre));
-
+			descendientes.add(new Persona(nombre,rut,edad,rut_madre));
+			descendientes.get(i).setRut_padre(rut_padre);
+			i++;
 	}
+
+
 		System.out.println(descendientes.get(0).getRut_madre());
 		File archivo_progenitoras = new File("progenitoras.txt");
 		Scanner archivo2 = new Scanner(archivo_progenitoras); //archivo progenitoras.
-		List<progenitoras> progenitoras = new ArrayList<progenitoras>();
-		int i = 0;
+		List<Persona> progenitoras = new ArrayList<Persona>();
+		i = 0;
 		int j = 0;
 		while (archivo2.hasNextLine()) {
 			
@@ -42,31 +47,74 @@ public class taller2 {
 			String rut = partes[2];
 			String rut_pareja = partes[3];
 			String rut_madre = partes[4];
-			progenitoras.add(new progenitoras(nombre,rut,edad,rut_madre,rut_pareja));
+			progenitoras.add(new Persona(nombre,rut,edad,rut_madre));
+			progenitoras.get(i).setRut_pareja(rut_pareja);
 			i++;
-			
-
-			}
-		System.out.println(progenitoras.size());
-		System.out.println(descendientes.size());
-		for(progenitoras progenitorass : progenitoras) {
+		}
+	System.out.println(descendientes);
 		
-			for (descendientes descendientess : descendientes) {
-				
-				
-				if (progenitorass.getRut().equals(descendientess.getRut_madre())) {
-								
-					progenitorass.setListaDescendientes(descendientess);
-				}
-				j++;
-
-		}
-			System.out.println(progenitoras.get(0).toString());
-		}
-		archivo1.close();
-		archivo2.close();
-		System.out.println("ola");
-
-			}
+	File archivo_progenitores = new File("progenitores.txt");
+	Scanner archivo3 = new Scanner(archivo_progenitores); //archivo progenitores.
+	List<Persona> progenitores = new ArrayList<Persona>();
+	i=0;
+	while (archivo3.hasNextLine()) {
+		
+	
+		String linea = archivo3.nextLine();
+		String[] partes = linea.split(",");
+		String nombre = partes[0];
+		
+		int edad = Integer.parseInt(partes[1]);
+		String rut = partes[2];
+		String rut_pareja = partes[4];
+		String rut_madre = partes[3];
+		progenitores.add(new Persona(nombre,rut,edad,rut_madre));
+		progenitores.get(i).setRut_pareja(rut_pareja);
+	}
+	System.out.println(progenitores);
+	}
 }
 
+
+//			
+//			}
+//		System.out.println(progenitoras.size());
+//		System.out.println(descendientes.size());
+//		
+//		
+//		for(progenitoras progenitorass : progenitoras) {
+//			
+//			for (progenitoras progenitorass2 : progenitoras) {
+//				
+//				
+//				if (progenitorass.getRut().equals(progenitorass2.getRut_madre())) {
+//								
+//					progenitorass.setListaDescendientes(progenitorass2);
+//				}
+//				j++;
+//
+//		}
+//		}
+//		System.out.println(progenitoras.get(1).getListaDescendientes());
+//		for(progenitoras progenitorass : progenitoras) {
+//		
+//			for (descendientes descendientess : descendientes) {
+//				
+//				
+//				if (progenitorass.getRut().equals(descendientess.getRut_madre())) {
+//								
+//					progenitorass.setListaDescendientes(descendientess);
+//				}
+//				j++;
+//
+//		}
+//
+//		}
+//		System.out.println(progenitoras.get(0).toString());
+//		archivo1.close();
+//		archivo2.close();
+//		System.out.println("ola");
+//
+//			}
+//}
+//
